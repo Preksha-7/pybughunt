fef863050220b0a3dfd74edd0ce132cdffe8e4dc
Preksha-7 <129003646+Preksha-7@users.noreply.github.com>
2025-04-06 00:41:11 +0530

    Initial commit

---
+++ src/code_error_detector/__init__.py
@@ -0,0 +1,6 @@
+"""Code Error Detector - A Python library for detecting logical and syntactical errors in Python code."""
+
+from .detector import CodeErrorDetector
+
+__version__ = "0.1.0"
+__all__ = ["CodeErrorDetector"]
